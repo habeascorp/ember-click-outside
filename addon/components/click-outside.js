d4fcfef30e078dddffd09ef55dd64d38350ee99c
Sheldon Baker <sheldonnbbaker@gmail.com>
2016-04-05 13:09:33 -0700

    Allow old-style param passing to component

--- addon/components/click-outside.js
+++ addon/components/click-outside.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 import ClickOutside from '../mixins/click-outside';
 import layout from '../templates/components/click-outside';
 import $ from 'jquery';
-const { Component, on } = Ember;
+const { Component, on, get } = Ember;
 const { next } = Ember.run;
 
 export default Component.extend(ClickOutside, {
   layout,
 
   clickOutside(e) {
-    const exceptSelector = this.attrs['except-selector'];
+    const exceptSelector = this.attrs && this.attrs['except-selector'] || get(this, 'except-selector');
     if (exceptSelector && $(e.target).closest(exceptSelector).length > 0) {
       return;
     }
